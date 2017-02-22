891b322694d02db667df869a943f6282297be1c5
Patrick <gwydo.official@googlemail.com>
2017-02-22 14:24:15 +0100

    Removed the comment again

--- src/hashcode/Hashcode.java
+++ src/hashcode/Hashcode.java
@@ -2,7 +2,6 @@ import java.io.FileNotFoundException;
 
 public class Hashcode {
 
-	//comment added by patrick to test git syncing
 	public static void main(String[] args) throws FileNotFoundException {
 		String filename = args[0];
 		Model model = Model.createModelFromFile(filename);
