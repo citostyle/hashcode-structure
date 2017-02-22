9e43e2bc0b48cb30320c893d0d71a9d1860bbedd
Patrick <gwydo.official@googlemail.com>
2017-02-22 14:15:58 +0100

    Testing Git Sync

--- src/hashcode/Hashcode.java
+++ src/hashcode/Hashcode.java
@@ -2,6 +2,7 @@ import java.io.FileNotFoundException;
 
 public class Hashcode {
 
+	//comment added by patrick to test git syncing
 	public static void main(String[] args) throws FileNotFoundException {
 		String filename = args[0];
 		Model model = Model.createModelFromFile(filename);
