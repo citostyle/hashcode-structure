3444bb2625e7cc92791b3ccfe060eae7b0f900a7
Patrick <gwydo.official@googlemail.com>
2017-02-23 20:39:41 +0100

    First Version of Greedy Strategy

---
+++ src/hashcode/GreedyStrategy.java
@@ -0,0 +1,48 @@
+package hashcode;
+
+import java.io.FileNotFoundException;
+import java.util.PriorityQueue;
+
+/**
+ * Created by Patrick on 23.02.17.
+ */
+public class GreedyStrategy {
+
+
+	public static void main(String[] args) throws FileNotFoundException {
+		if(args.length == 0) {
+			System.out.println("Provide an input file for us to solve");
+		}
+		String filename = args[0];
+		Model model = Model.createModelFromFile(filename);
+		System.out.println("done");
+
+
+		boolean videoPlaced = true;
+		while (videoPlaced){
+			videoPlaced = false;
+			for(CacheServer cacheServer: model.cacheServers) {
+				Video bestVideo = null;
+				double bestVideosScore = Double.MAX_VALUEN
+				for (Video video : model.videos) {
+					double score = 0;
+					if (video.size <= cacheServer.remainingCapacity){
+						for(Endpoint endpoint : cacheServer.connectedEndpoints){
+							score += endpoint.requests * Math.max(0,model.getCurrentDelay(video,endpoint));
+						}
+					}
+					score/=video.size;
+				}
+			}
+		}
+
+
+
+
+
+
+	}
+
+
+
+}
