db37ad127115292d7919fcde7ee8fb50ddf05318
lmascarell <lmascarell.espuny@gmail.com>
2017-02-23 20:11:48 +0100

    totalscore

--- src/hashcode/Model.java
+++ src/hashcode/Model.java
@@ -11,7 +11,36 @@ public class Model {
 	List<Endpoint> endpoints;
 	
 	
-	
+	public int getTotalScore() {
+
+        for (Endpoint e: endpoints) {
+            for (Map.Entry<Video, Integer> videoRequest : e.requests.entrySet()) { // each endpoint
+                Video currentVideo = videoRequest.getKey();
+                Integer currentVideoRequests = videoRequest.get
+                List<Integer> latencies;
+                latencies.add(e.latencyDataCenter);
+                for(Map.Entry<CacheServer, Integer> cacheLatencies: e.cacheLatencies.entrySet()) { // each cache connected to endpoint
+                    CacheServer cache = cache.getKey();
+                    Integer latencyCache = cache.getValue();
+                    for(Video cacheVideo: cache.assignment) { // videos assignes to that cache
+                        if(cacheVideo.videoId == videoRequest.getKey()) {// if the cache has the video
+                            latencies.add(latencyCache);
+                            break;
+                        }
+                    }
+                }
+                System.out.println(entry.getKey() + "/" + entry.getValue());
+            }
+        }
+		// for each endpoint
+            // for each video that has this video
+                //for each cache that has the video to that endpoint
+                // L = min(LD, latencies caches)
+                // Requests x (LD-L)
+
+            //Total is the (sum(time saved individual request descriptions * 1000) / total number of requests in all requests //rounding down
+
+	}
 	
 	//example code of how to read model parameters from files 
 	public static Model createModelFromFile(String filename) throws FileNotFoundException {
