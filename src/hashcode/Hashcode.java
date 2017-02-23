2a2882063e813da252b0eaada3849a8b05abd202
lisgie <livio@localhost.localdomain>
2017-02-23 17:53:19 +0100

    Push testing

--- src/hashcode/Hashcode.java
+++ src/hashcode/Hashcode.java
@@ -5,6 +5,7 @@ public class Hashcode {
 	public static void main(String[] args) throws FileNotFoundException {
 		String filename = args[0];
 		Model model = Model.createModelFromFile(filename);
+		
 	}
 
 }
