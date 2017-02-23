322e7b5306776cac723b2a43b17511b41095a3fc
lmascarell <lmascarell.espuny@gmail.com>
2017-02-23 20:11:15 +0100

    assignment

--- src/hashcode/CacheServer.java
+++ src/hashcode/CacheServer.java
@@ -4,7 +4,9 @@ public class CacheServer {
 	// keep it now public for ease
 	public int cacheServerId;
 	public int maxCapacity;
-	
+	public List<Video> assignment;
+
+
 	public CacheServer(int cacheServerId, int maxCapacity) {
 		super();
 		this.cacheServerId = cacheServerId;
